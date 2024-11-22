a276496882134f06dff4837842c38cd73a76d781
uwuowoayaya <wiktor0204@gmail.com>
2024-11-22 18:28:35 +0100

    Dodano funkcję do robienia wykresów

--- MatlabDoubleIntegralValue.m
+++ MatlabDoubleIntegralValue.m
@@ -6,10 +6,14 @@ function q = MatlabDoubleIntegralValue(f,tol)
 % f na obszarze D za pomocą wbudowanej funkcji integral2
 % Funkcja przyjmuje dwa argumenty:
 % f - uchwyt do funkcji podcałkowej
-% tol - tolerancja funkcji integral2
+% tol - tolerancja funkcji integral2, domyślnie tol = 1e-9
 % Obszar D jest dzielony na dwa trójkąty:
 % 1) lewy, ograniczony przez y = -x-1, y = x+1, x = 0
 % 2) prawy, ograniczony przez y = x-1, y = -x+1, x = 0
+
+if nargin < 2
+  tol = 1e-9;
+end
 
 % Obliczenie całki na lewym trójkącie
 qleft = integral2(f,-1,0,@(x) -x-1,@(x) x+1,'AbsTol',tol);
